e2654e2635180cabd921c70de6d3082a684b35be
lupita alvarez <alv.lu1305@gmail.com>
2021-12-10 11:42:59 -0800

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,34 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+ const getAll = async () => {
+ 	const projects = await db('projects')
+ 	projects.forEach(project => {
+ 		if (project.project_completed == 0) {
+ 			project.project_completed = false
+ 		} else {
+ 			project.project_completed = true
+ 		}
+ 	})
+ 	return projects
+ }
+ const getById = async project_id => {
+ 	const [project] = await db('projects').where('project_id', project_id)
+ 	if (project.project_completed == 0) {
+ 		project.project_completed = false
+ 	} else {
+ 		project.project_completed = true
+ 	}
+ 	return project
+ }
+ const create = async project => {
+ 	const [id] = await db('projects').insert(project)
+ 	const newProject = await getById(id)
+ 	return newProject
+ }
+
+ module.exports = {
+ 	getAll,
+ 	getById,
+ 	create
+ }
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,24 @@
 // build your `/api/projects` router here
+const express = require('express')
+const router = express.Router()
+const Projects = require('./model')
+const {
+checkProjectPayload
+} = require('./middleware')
+
+router.get('/', async (req, res) => {
+await Projects.getAll(req.query)
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+})
+
+router.post('/', checkProjectPayload, async (req, res) => {
+await Projects.create(req.body)
+    .then(newProject => {
+        res.status(201).json(newProject)
+    })
+})
+
+
+module.exports = router
